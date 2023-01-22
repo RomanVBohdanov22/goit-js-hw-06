286b8add2dc5a4c4edac604d18a7b2f7120adbb8
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-23 00:06:01 +0200

    think about 06-01

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,8 @@
 Напиши скрипт, який:
 
     Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-    Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+    Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку 
+    елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
 В результаті, в консолі будуть виведені наступні повідомлення.
 
@@ -33,3 +34,24 @@ console.log(firstListItem);
 
 const listItems = list.children;
 console.log(listItems);
+
+/*
+console.log('');
+
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '24px';
+console.log(listWithId);
+
+const listWithClass = document.querySelector('.menu');
+console.log(listWithClass);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = 'tomato';
+console.log(firstMenuItem); */
