07859c5ce59e585df0fab9a49dd783afa8d581b1
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-02 00:22:19 +0200

    task 5 complete

--- js/task-05.js
+++ js/task-05.js
@@ -8,3 +8,23 @@
 <input type="text" id="name-input" placeholder="Please enter your name" />
 <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 */
+
+/*
+    <input type="text" id="name-input" placeholder="Please enter your name" />
+    <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+*/
+
+const nameInputLnk = document.querySelector('#name-input');
+const nameOutputLnk = document.querySelector('#name-output');
+const valueDefault = 'Anonymous';
+
+nameInputLnk.addEventListener('input', changeOutput);
+
+function changeOutput() { 
+    
+    if (nameInputLnk.value === '') {
+        nameOutputLnk.textContent = valueDefault;
+        return;
+    };
+    nameOutputLnk.textContent = nameInputLnk.value;
+}
